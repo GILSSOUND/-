558a4278bed4b55ecf002832087db52f4b826bcf
GILSSOUND <sgd41990@gmail.com>
2026-08-09 15:11:51 +0900

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -17,6 +17,9 @@ require('./config/passport')();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Trust the proxy (Render uses a reverse proxy) to correctly identify https
+app.set('trust proxy', 1);
+
 app.use(cors({
   origin: true,
   credentials: true
