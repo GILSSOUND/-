43b3a562769c363cbe98b90e11ffc91f1fc1c00a
GILSSOUND <sgd41990@gmail.com>
2026-09-07 19:34:10 +0900

    Add files via upload

--- src/api.js
+++ src/api.js
@@ -57,11 +57,11 @@ const compressImage = (file) => {
         
         canvas.toBlob((blob) => {
           if (!blob) return resolve(file);
-          resolve(new File([blob], file.name.replace(/\.[^/.]+$/, "") + ".webp", {
-            type: 'image/webp',
+          resolve(new File([blob], file.name.replace(/\.[^/.]+$/, "") + ".jpeg", {
+            type: 'image/jpeg',
             lastModified: Date.now()
           }));
-        }, 'image/webp', 0.8);
+        }, 'image/jpeg', 0.85);
       };
       img.onerror = () => resolve(file);
     };
